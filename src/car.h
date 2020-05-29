279dec848fda761c67102a4229c6af5e7e9ab3f7
Paul Yeh <pyeh.tw@icloud.com>
2020-05-29 16:10:11 +0200

    skeleton codes

---
+++ src/car.h
@@ -0,0 +1,18 @@
+#ifndef CAR_H
+#define CAR_H
+
+class Car
+{
+    ~Car();
+    Car();
+
+public:
+    double x;
+    double y;
+    double s;
+    double d;
+    double v;
+    double yaw;
+};
+
+#endif // CAR_H
---
+++ src/path_planner.h
@@ -0,0 +1,10 @@
+#ifndef PATH_PLANNER_H
+#define PATH_PLANNER_H
+
+class PathPlanner
+{
+    ~PathPlanner();
+    PathPlanner();
+};
+
+#endif // PATH_PLANNER_H
